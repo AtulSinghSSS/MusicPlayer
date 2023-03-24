8aca418e6b204b4a7daf3e7f99fabbadf0e8b2dd
AtulSinghSk <atul@shopkirana.com>
2023-03-24 12:49:39 +0530

    change coade

--- app/src/main/java/com/example/firebaseproject/fragment/ExploreFragment.kt
+++ app/src/main/java/com/example/firebaseproject/fragment/ExploreFragment.kt
@@ -1,60 +1,208 @@
 package com.example.firebaseproject.fragment
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.firebaseproject.R
+import com.example.firebaseproject.activity.HomeActivity
+import com.example.firebaseproject.appRepository.MyApplication
+import com.example.firebaseproject.databinding.FragmentExploreBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ExploreFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ExploreFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    lateinit var mBinding:FragmentExploreBinding
+    lateinit var activity: HomeActivity
+    lateinit var appCtx: MyApplication
+    lateinit var mediaPlayer: MediaPlayer
+    lateinit var runnable:Runnable
+    lateinit var handler: Handler
+    var pause:Boolean = false
+    var firstAttempt:Boolean = true
+    val myArrayList = ArrayList<String>()
+    var songs: MutableList<String> = ArrayList()
+     var nowPlaying=0
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = context as HomeActivity
+        appCtx = activity.application as MyApplication
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        mBinding=DataBindingUtil.inflate(inflater,R.layout.fragment_explore, container, false)
+        handler= Handler()
+        CallFun()
+        return mBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ExploreFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExploreFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun CallFun() {
+      /*  var uriStr = "android.resource://"+ "com.example.phone"+ "/" + "raw/file_example_audio";
+        var uri = Uri.parse(uriStr)
+        mediaPlayer = MediaPlayer.create(activity,uri)*/
+
+        var selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
+        var projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.DISPLAY_NAME,
+            MediaStore.Audio.Media.DURATION
+        )
+
+       var cursor = activity.managedQuery(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null, null
+        )
+
+        while (cursor.moveToNext()) {
+            songs.add((cursor.getString(0)+ cursor.getString(1)))
+        }
+        // Start the media player
+       mBinding.llplay.setOnClickListener{
+            if(pause){
+                mBinding.pauseBtn.visibility=View.VISIBLE
+                mBinding.llplay .visibility=View.GONE
+                mediaPlayer.seekTo(mediaPlayer.currentPosition)
+                mediaPlayer.start()
+                pause = false
+                Toast.makeText(activity,"media playing",Toast.LENGTH_SHORT).show()
+            }else{
+                mBinding.pauseBtn.visibility=View.VISIBLE
+                mBinding.llplay .visibility=View.GONE
+                mediaPlayer = MediaPlayer.create(activity.applicationContext,R.raw.file_example_audio)
+                mediaPlayer.start()
+                Toast.makeText(activity,"media playing",Toast.LENGTH_SHORT).show()
+
+            }
+           initializeSeekBar()
+           mBinding.llplay.isEnabled = false
+           mBinding.pauseBtn.isEnabled = true
+           mBinding.stopBtn.isEnabled = true
+
+           mediaPlayer.setOnCompletionListener {
+               mBinding.llplay.isEnabled = true
+              mBinding.pauseBtn.isEnabled = false
+               mBinding.stopBtn.isEnabled = false
+               Toast.makeText(activity,"end",Toast.LENGTH_SHORT).show()
+           }
+       }
+
+        // Pause the media player
+        mBinding.pauseBtn.setOnClickListener {
+            if(mediaPlayer.isPlaying){
+                mBinding.pauseBtn.visibility=View.GONE
+                mBinding.llplay .visibility=View.VISIBLE
+                mediaPlayer.pause()
+                pause = true
+                mBinding.llplay.isEnabled = true
+                mBinding.pauseBtn.isEnabled = false
+                mBinding.stopBtn.isEnabled = true
+                Toast.makeText(activity,"media pause",Toast.LENGTH_SHORT).show()
+            }
+        }
+        mBinding.llNext.setOnClickListener{
+
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+            mediaPlayer.setOnCompletionListener {
+                try {
+                    if (firstAttempt) {
+                        firstAttempt = false
+                    } else {
+                        nextSong()
+                        mediaPlayer.start()
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
             }
+
+            if(firstAttempt){
+                firstAttempt = false;
+            }else{
+             //   nextSong();
+                mediaPlayer.start()
+                Toast.makeText(activity,"media Next",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Stop the media player
+        mBinding.stopBtn.setOnClickListener{
+            if(mediaPlayer.isPlaying || pause.equals(true)){
+                mBinding.pauseBtn.visibility=View.GONE
+                mBinding.llplay .visibility=View.VISIBLE
+                pause = false
+                mBinding.seekBar.setProgress(0)
+                mediaPlayer.stop()
+                mediaPlayer.reset()
+                mediaPlayer.release()
+                handler.removeCallbacks(runnable)
+                mBinding.llplay.isEnabled = true
+                mBinding.pauseBtn.isEnabled = false
+                mBinding.stopBtn.isEnabled = false
+                mBinding.tvPass.text = ""
+                mBinding.tvDue.text = ""
+                Toast.makeText(activity,"media stop",Toast.LENGTH_SHORT).show()
+            }
+        }
+        // Seek bar change listener
+        mBinding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                if (b) {
+                    mediaPlayer.seekTo(i * 1000)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
     }
+
+    private fun initializeSeekBar() {
+        mBinding.seekBar.max = mediaPlayer.seconds
+
+        runnable = Runnable {
+            mBinding.seekBar.progress = mediaPlayer.currentSeconds
+
+            mBinding.tvPass.text = "${mediaPlayer.currentSeconds} sec"
+            val diff = mediaPlayer.seconds - mediaPlayer.currentSeconds
+            mBinding.tvDue .text = "$diff sec"
+
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    fun nextSong() {
+        nowPlaying = nowPlaying + 1
+        if (nowPlaying === songs.size) {
+            nowPlaying = 0
+        }
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.start()
+        }
+    }
+
+    // Creating an extension property to get the media player time duration in seconds
+    val MediaPlayer.seconds:Int
+        get() {
+            return this.duration / 1000
+        }
+    // Creating an extension property to get media player current position in seconds
+    val MediaPlayer.currentSeconds:Int
+        get() {
+            return this.currentPosition/1000
+        }
 }
